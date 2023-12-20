270f389e6eb1c08401eda0278ff8fd647e96546a
Maksym Harapko <harapkomaskym@gmail.com>
2023-12-20 17:48:47 +0200

    Initial commit

---
+++ Laba10 V5/IQuadratic.cs
@@ -0,0 +1,14 @@
+namespace Laba10_V5;
+
+public interface IQuadratic
+{
+    private double Discriminator()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Roots()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Laba10 V5/QuadraticEquation.cs
+++ Laba10 V5/QuadraticEquation.cs
@@ -1,13 +1,13 @@
 namespace Laba10_V5;
 
-public class QuadraticEquation
+public class QuadraticEquation : IQuadratic
 {
     private double _a;
     private double _b;
     private double _c;
 
-    public double X1 { get; private set; }
-    public double X2 { get; private set; }
+    public string X1 { get; private set; }
+    public string X2 { get; private set; }
 
     public QuadraticEquation(double a, double b, double c)
     {
@@ -23,10 +23,19 @@ public class QuadraticEquation
 
     private void Roots()
     {
-        X1 = (-_b + Math.Sqrt(Discriminator())) / (2 * _a);
-        X2 = (-_b - Math.Sqrt(Discriminator())) / (2 * _a);
+        if (Discriminator() > 0)
+        {
+            X1 = ((-_b + Math.Sqrt(Discriminator())) / (2 * _a)).ToString();
+            X2 = ((-_b - Math.Sqrt(Discriminator())) / (2 * _a)).ToString();
+        }
+        else if(Discriminator() < 0)
+        {
+            X1 = Convert.ToString($"{-_b} + {Math.Round(Math.Sqrt(Discriminator() * -1), 1)}i / {2 * _a}");
+            X2 = Convert.ToString($"{-_b} - {Math.Round(Math.Sqrt(Discriminator() * -1), 1)}i / {2 * _a}");
+        }
+        
     }
 
     public override string ToString() =>
-        $"x1 = {Math.Round(X1, 1)}\nx2 = {Math.Round(X2, 1)}";
+        $"x1 = {X1}\nx2 = {X2}";
 }
